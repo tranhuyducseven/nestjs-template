90f0f4c3b51a33c317b322db664fa0a7b5b7a660
tranhuyducseven <tranhuyducseven@gmail.com>
2023-06-12 15:38:56 +0700

    feature/file: upload a file api

--- src/modules/file/controllers/file.controller.ts
+++ src/modules/file/controllers/file.controller.ts
@@ -1,18 +1,32 @@
-import {Post, UploadedFile, UseInterceptors} from "@nestjs/common";
-import {FileInterceptor} from "@nestjs/platform-express";
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { extname } from "path";
 
-export class FileController{
-  @Post()
+@Controller("file")
+export class FileController {
+  @Post("/upload")
   @UseInterceptors(
-    FileInterceptor('text', {
-      dest:"../files"
-    }),
+    FileInterceptor("file", {
+      storage: diskStorage({
+        destination: "./files",
+        filename: (_, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + "-" + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    })
   )
-  async uploadedFile(@UploadedFile() file: any) {
-    return {
-      originalname: file.originalname,
-      filename: file.filename,
-    };
+  handleUpload(@UploadedFile() file: Express.Multer.File) {
+    console.log("file", file);
+    return "File upload API";
   }
-
 }
--- src/modules/file/file.module.ts
+++ src/modules/file/file.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { MulterModule } from '@nestjs/platform-express';
-import {FileController} from "./controllers/file.controller";
+import { MulterModule } from "@nestjs/platform-express";
+import { FileController } from "./controllers/file.controller";
+import { Module } from "@nestjs/common";
 
 @Module({
   imports: [MulterModule.register({
